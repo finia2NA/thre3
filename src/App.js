7811bac32cb36851d0629a10be5afaff95a463dd
finia2na <undifini@gmail.com>
2021-03-28 12:14:34 +0200

    apply auto formatting on commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { useRef, useState, useMemo, useEffect } from "react";
 import { Canvas, useFrame, useThree, extend } from "react-three-fiber";
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import Box from "./components/Box"
-import Controlpanel from "components/ControlPanel"
+import Box from "components/Box";
+import Controlpanel from "components/ControlPanel";
 
-extend({ OrbitControls })
+extend({ OrbitControls });
 
 const App = () => {
-
-
   return (
-    <div style={{
-      display: 'flex',
-      flexBasis: 'row',
-      padding: '10px'
-    }}>
-      <div style={{ display: 'flex', flex: 4 }}>
+    <div
+      style={{
+        display: "flex",
+        flexBasis: "row",
+        padding: "10px",
+      }}
+    >
+      <div style={{ display: "flex", flex: 4 }}>
         <Canvas
           camera={{ fov: 75, position: [1, 0, 0] }}
           onCreated={({ gl }) => {
-            gl.setClearColor('darkgrey')
+            gl.setClearColor("darkgrey");
           }}
         >
           <ambientLight intensity={0.5} />
           <Box position={[-1.2, 0, 0]} />
         </Canvas>
       </div>
-      <div style={{ display: 'flex', flex: 3 }}></div>
+      <div style={{ display: "flex", flex: 3 }}></div>
       <Controlpanel />
     </div>
   );
-}
+};
 
 export default App;
