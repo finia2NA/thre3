f873813c12491cd8d6f3099143ce7070d1b52feb
Richard Hanß <stu209194@mail.uni-kiel.de>
2021-04-13 06:03:05 +0200

    add continuity option to explore

--- src/components/UI/Explore.js
+++ src/components/UI/Explore.js
@@ -7,34 +7,72 @@ import {
   FormLabel,
   RadioGroup,
   Button,
-  Radio
-} from "@material-ui/core"
+  Radio,
+} from "@material-ui/core";
 
 const Explorepanel = (props) => {
-
-  const [viewMode, setView] = useState(0)
-  const [compareMethod, setMethod] = useState(0)
+  const [viewMode, setView] = useState(0);
+  const [compareMethod, setMethod] = useState(0);
 
   return (
     <div>
       <FormLabel>Mode</FormLabel>
       <RadioGroup value={viewMode}>
-        <FormControlLabel value={0} control={<Radio />} label="View" onClick={() => setView(0)} />
-        <FormControlLabel value={1} control={<Radio />} label="Patches" onClick={() => setView(1)} />
-        <FormControlLabel value={2} control={<Radio />} disabled={!props.prog_available} label="Diff" onClick={() => setView(2)} />
-        <FormControlLabel value={3} control={<Radio />} disabled={!props.prog_available} label="Unshot Radiosity" onClick={() => setView(3)} />
+        <FormControlLabel
+          value={0}
+          control={<Radio />}
+          label="View"
+          onClick={() => setView(0)}
+        />
+        <FormControlLabel
+          value={1}
+          control={<Radio />}
+          label="Patches"
+          onClick={() => setView(1)}
+        />
+        <FormControlLabel
+          value={4}
+          control={<Radio />}
+          label="Continuity"
+          onClick={() => setView(4)}
+        />
+        <FormControlLabel
+          value={2}
+          control={<Radio />}
+          disabled={!props.prog_available}
+          label="Diff"
+          onClick={() => setView(2)}
+        />
+        <FormControlLabel
+          value={3}
+          control={<Radio />}
+          disabled={!props.prog_available}
+          label="Unshot Radiosity"
+          onClick={() => setView(3)}
+        />
       </RadioGroup>
 
-      {viewMode === 2 &&
+      {viewMode === 2 && (
         <>
           <FormLabel>Diff Type</FormLabel>
           <RadioGroup value={compareMethod}>
-            <FormControlLabel value={0} control={<Radio />} disabled={!props.matrix_available} label="Intermethodical" onClick={() => setMethod(0)} />
-            <FormControlLabel value={1} control={<Radio />} label="Intraprogressive" onClick={() => setMethod(1)} />
+            <FormControlLabel
+              value={0}
+              control={<Radio />}
+              disabled={!props.matrix_available}
+              label="Intermethodical"
+              onClick={() => setMethod(0)}
+            />
+            <FormControlLabel
+              value={1}
+              control={<Radio />}
+              label="Intraprogressive"
+              onClick={() => setMethod(1)}
+            />
           </RadioGroup>
         </>
-      }
-      {viewMode === 2 && compareMethod === 1 &&
+      )}
+      {viewMode === 2 && compareMethod === 1 && (
         <>
           <FormLabel>Iterations to Compare</FormLabel>
           <Slider
@@ -45,8 +83,8 @@ const Explorepanel = (props) => {
             valueLabelDisplay="auto"
           />
         </>
-      }
-      {(viewMode === 3 || (viewMode === 2 && compareMethod === 0)) &&
+      )}
+      {(viewMode === 3 || (viewMode === 2 && compareMethod === 0)) && (
         <>
           <FormLabel>Progressive Iteration</FormLabel>
           <Slider
@@ -56,13 +94,12 @@ const Explorepanel = (props) => {
             valueLabelDisplay="auto"
           />
         </>
+      )}
+      {
+        viewMode === 2 && <Button variant="contained">Compare</Button> // since comparisons need new textures, I don't want the dragging of the slider to instantly trigger the comparison for performance reasons.
       }
-      {viewMode === 2 &&
-        <Button variant="contained">Compare</Button> // since comparisons need new textures, I don't want the dragging of the slider to instantly trigger the comparison for performance reasons.
-      }
-
     </div>
   );
-}
+};
 
-export default Explorepanel
+export default Explorepanel;
