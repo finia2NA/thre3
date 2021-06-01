5d7e656e832acccd801a170716a1f4e8eacd3512
Richard Hanß <stu209194@mail.uni-kiel.de>
2021-06-01 10:44:41 +0200

    Data Structure for commutative operations (python)

--- random/symmetrical/sym.py
+++ random/symmetrical/sym.py
@@ -1,11 +1,33 @@
+from functools import reduce
+
+
 class Sym:
 
-  def __init__(self, sqareDim):
-    self.array = [[0 for _ in range(i)] for i in map(
-        lambda x:sqareDim-x, range(sqareDim))]
+  def __init__(self, dimensions: list[int]):
+    self.dimensions = dimensions
+
+    maxIndex = reduce(lambda x, y: x*y, dimensions)
+    self.array = \
+        [[0 for col in range(row, maxIndex)] for row in range(maxIndex)]
+
+  def __getIndex__(self, a):
+    # 1:1 relation between index and position in hypercube.
+    assert len(a) == len(self.dimensions)
+    for i in range(len(a)):
+      assert a[i] < self.dimensions[i]
+
+    re = 0
+
+    for i in range(len(a)-1):
+      re += a[i]*self.dimensions[i+1]
+
+    re += a[len(a)-1]
+
+    return re
 
   def __getIndices__(self, a, b):
-    return sorted([a, b])
+    s = sorted([a, b])
+    return [self.__getIndex__(s[0]), self.__getIndex__(s[1])]
 
   def get(self, a, b):
     indices = self.__getIndices__(a, b)
@@ -15,15 +37,19 @@ class Sym:
   def set(self, a, b, value):
     indices = self.__getIndices__(a, b)
 
-    akali = self.array[indices[0]]
     self.array[indices[0]][indices[1]] = value
 
 
 if __name__ == "__main__":
-  testSym = Sym(5)
+  dim = [5, 16, 16]
+
+  a = [4, 12, 2]
+  b = [0, 3, 15]
+
+  testSym = Sym(dim)
+
+  testSym.set(a, b, "hello!")
   print(testSym.array)
 
-  testSym.set(0, 4, "hello!")
-
-  print(testSym.get(4, 0))
-  print(testSym.get(0, 4))
+  print(testSym.get(a, b))
+  print(testSym.get(b, a))
