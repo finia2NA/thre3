1c43382a448ed855935babf7fde9137ff2ddf04a
Richard Hanß <stu209194@mail.uni-kiel.de>
2021-06-20 21:15:42 +0200

    patch Texture

--- src/components/3D/textures.js
+++ src/components/3D/textures.js
@@ -65,12 +65,10 @@ export const patchTexture = (patches, width, height) => {
   var canvas = new OffscreenCanvas(width, height);
   var context = canvas.getContext("2d");
 
-  // Turned off right now, in theory no area that is not calculated should be visible anyway, so it doesnt matter what color the textuer is there.
-  // bg of the canvas is no energy = black
+  // Turned off right now, in theory no area that is not calculated should be visible anyway, so it doesnt matter what color the texture is in areas that are not covered by patches.
   // context.fillStyle = "black";
-  // // FIXME: needs to be of some weird type.
   // // TODO: off-by-ones would lead to overdrawing or to wite line at the edge
-  // context.fill(0, 0, width, height);
+  // context.fillRect(0, 0, width, height);
 
   // determine the max energy in a single color channel.
   const maxComponents = new Vector3(0, 0, 0);
@@ -84,26 +82,30 @@ export const patchTexture = (patches, width, height) => {
   }
   const maxBrightness = Math.max(...maxComponents.toArray());
 
-  debugger;
-
   for (var i = 0; i < patches.length; i++) {
     for (var j = 0; j < patches[i].length; j++) {
+      if (!patches[i][j]) {
+        // if there's no patch here there's nothing to do, eh? ^^
+        continue;
+      }
       const patch = patches[i][j];
-      const wattage = patch.displayEnergy;
-
-      debugger;
+      const wattage = patch.displayEnergy
+        .clone()
+        .multiplyScalar(255 / maxBrightness);
 
       context.fillStyle =
         "#" +
         rgbHex(
-          Math.round(wattage[0] / maxBrightness),
-          Math.round(wattage[1] / maxBrightness),
-          Math.round(wattage[2] / maxBrightness)
+          Math.round(wattage.x),
+          Math.round(wattage.y),
+          Math.round(wattage.z)
         );
-      context.fillRect(patch.backwriteCoord[0], patch.backwriteCoord[1], 1, 1);
-      return canvas.transferToImageBitmap();
+
+      context.fillRect(patch.positionTX[0], patch.positionTX[1], 1, 1);
     }
   }
+
+  return canvas.transferToImageBitmap();
 };
 
 export const defaultTexture = (width = 32, height = 32) => {
