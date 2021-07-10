50c70e027dd0d5f530fa4fa522e7a1a4ca96430f
Richard Hanss <stu209194@mail.uni-kiel.de>
2021-07-10 20:04:19 +0200

    hemisphere sample point generator

--- src/formFactors/hemiSample.js
+++ src/formFactors/hemiSample.js
@@ -1,10 +1,12 @@
-export function getSphereSamplepoints(samples) {
-  //stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere/26127012#26127012
+import { Vector3 } from "three";
+
+export function getSphereSamplepoints(endsample, startSample = 0) {
+  // https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere/26127012#26127012
   https: var points = [];
   const phi = Math.PI * (3 - Math.sqrt(5)); // golden angle in radians
 
-  for (var i = 0; i < samples; i++) {
-    const y = 1 - (i / samples - 1) * 2; // y goes from 1 to - 1
+  for (var i = startSample; i < endsample; i++) {
+    const y = 1 - (i / (endsample - 1)) * 2; // y goes from 1 to - 1
     const radius = Math.sqrt(1 - y * y); // radius at y
 
     const theta = phi * i; // golden angle increment
@@ -12,12 +14,21 @@ export function getSphereSamplepoints(samples) {
     const x = Math.cos(theta) * radius;
     const z = Math.sin(theta) * radius;
 
-    points.append((x, y, z));
+    points.push(new Vector3(x, y, z));
   }
   return points;
 }
 
 export default function getHemisphereSamplepoints(samples) {
-  // adapted from prev. method, with adjustments from https://stackoverflow.com/questions/34313200/distribute-points-evenly-on-a-unit-hemisphere
-  // TODO: implement
+  // adapted using prev. method from https://stackoverflow.com/questions/34313200/distribute-points-evenly-on-a-unit-hemisphere
+  const points = getSphereSamplepoints(2 * samples, samples);
+
+  for (const p of points) {
+    p.negate();
+
+    // just to be sure...
+    if (p.y < 0) console.error("hemisphere sample point not in hemisphere");
+  }
+
+  return points;
 }
